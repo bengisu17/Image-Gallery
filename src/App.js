8aeaae4472aff3ae3075c1c8ee386fbfbc8c52eb
bengisu17 <bengisuyavuz17@gmail.com>
2022-10-11 16:44:35 +0300

    done project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './component/Navbar/Navbar'
+import Header from './component/Header/Header'
+import Main from './component/Main/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
